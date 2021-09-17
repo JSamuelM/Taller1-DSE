cfae256fe4c9d581eb722cd07b0eac9c6d380653
Samuel Mena <samuel23mena@gmail.com>
2021-09-16 22:25:45 -0600

    Add project files.

---
+++ Taller1MR171621/Controllers/ProductosController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Taller1MR171621.Models;
+
+namespace Taller1MR171621.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private ProductoDBContext db = new ProductoDBContext();
+
+        // GET: Productos
+        public ActionResult Index(string description, int exis = -1, decimal precioVenta = -1)
+        {
+            var productos = from p in db.Productos
+                            select p;
+
+            if (!string.IsNullOrEmpty(description))
+            {
+                productos = productos.Where(d => d.Description.Contains(description));
+            }
+
+            if (exis > 0)
+            {
+                productos = productos.Where(e => e.Existencia == exis);
+            }
+
+            if (precioVenta > 0)
+            {
+                productos = productos.Where(p => p.PrecioVenta == precioVenta);
+            }
+
+            return View(productos);
+        }
+
+        // GET: Productos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Productos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Productos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Description,Categoria,Costo,PrecioVenta,Existencia,NumeroPedidos")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productos.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(producto);
+        }
+
+        // GET: Productos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Productos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Description,Categoria,Costo,PrecioVenta,Existencia,NumeroPedidos")] Producto producto)
+        {
+            if (producto.NumeroPedidos > producto.Existencia)
+            {
+                ModelState.AddModelError("", "El número de pedidos no puede ser mayor al número de existencias");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(producto);
+        }
+
+        // GET: Productos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Productos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            db.Productos.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Taller1MR171621/Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace Taller1MR171621.Models
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+        public string Categoria { get; set; }
+        public decimal Costo { get; set; }
+        [Display(Name = "Precio de Venta")]
+        public decimal PrecioVenta { get; set; }
+        public int Existencia { get; set; }
+        [Display(Name = "Número de Pedidos")]
+        public int NumeroPedidos { get; set; }
+    }
+
+    public class ProductoDBContext : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
